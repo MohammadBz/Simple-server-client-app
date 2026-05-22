c24485792d0b8874338d0aae13937d0e14059de2
MohammadBazazzadeh <m.bazazzadeh84@gmail.com>
2026-05-23 01:10:35 +0330

    refactor(Exception): InvalidCredentialsException now carry username

--- src/main/java/exception/InvalidCredentialsException.java
+++ src/main/java/exception/InvalidCredentialsException.java
@@ -1,7 +1,14 @@
 package exception;
 
 public class InvalidCredentialsException extends AuthenticationException {
-    public InvalidCredentialsException(String message) {
+    private final String username;
+
+    public InvalidCredentialsException(String message, String username) {
         super(message);
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
     }
 }
