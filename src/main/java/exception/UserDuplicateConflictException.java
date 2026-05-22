d085d8e39827f4a7b7cafbfc25a907ead7cef3c1
MohammadBazazzadeh <m.bazazzadeh84@gmail.com>
2026-05-23 00:25:56 +0330

    refactor(Exception): UserDuplicateConflictException now carry username

--- src/main/java/exception/UserDuplicateConflictException.java
+++ src/main/java/exception/UserDuplicateConflictException.java
@@ -1,7 +1,14 @@
 package exception;
 
 public class UserDuplicateConflictException extends RuntimeException {
-    public UserDuplicateConflictException(String message) {
+    private final String username;
+
+    public UserDuplicateConflictException(String message, String username) {
         super(message);
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
     }
 }
