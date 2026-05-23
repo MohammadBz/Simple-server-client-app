35f094af8c3cfddadafe4221913a97566d29faca
MohammadBazazzadeh <m.bazazzadeh84@gmail.com>
2026-05-23 18:32:26 +0330

    refactor(Exception): introduce SystemErrorResolver

---
+++ src/main/java/exception/SystemErrorResolver.java
@@ -0,0 +1,11 @@
+package exception;
+
+import service.core.ClientHandler;
+
+public class SystemErrorResolver implements ErrorResolver {
+
+    @Override
+    public void resolve(Exception e, ClientHandler client) {
+
+    }
+}
